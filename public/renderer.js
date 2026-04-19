959b9304cb5126fcb42f87d1046ad3f8a5cf06a3
Katsumi <futak1192@gmail.com>
2026-04-20 10:59:22 +1200

    Refactor clock modules: replace clock6.js with rolling.js, clock3.js with flip.js, clock4.js with minimal-analog.js, and clock7.js with grid.js; remove obsolete files and update renderer.js to reflect changes.

--- public/renderer.js
+++ public/renderer.js
@@ -59,13 +59,13 @@ const fontFamilies = {
 
 const clockModules = {
   0: { globalName: "renderClock5", src: "clocks/clock1/binary.js" },
-  1: { globalName: "renderClock6", src: "clocks/clock2/clock6.js" },
+  1: { globalName: "renderClock6", src: "clocks/clock2/rolling.js" },
   2: { globalName: "renderClock8", src: "clocks/clock3/clock8.js" },
   3: { globalName: "renderClock1", src: "clocks/clock4/digital.js" },
   4: { globalName: "renderClock2", src: "clocks/clock5/analog.js" },
-  5: { globalName: "renderClock3", src: "clocks/clock6/clock3.js" },
-  6: { globalName: "renderClock4", src: "clocks/clock7/clock4.js" },
-  7: { globalName: "renderClock7", src: "clocks/clock8/clock7.js" },
+  5: { globalName: "renderClock3", src: "clocks/clock6/flip.js" },
+  6: { globalName: "renderClock4", src: "clocks/clock7/minimal-analog.js" },
+  7: { globalName: "renderClock7", src: "clocks/clock8/grid.js" },
 };
 
 const styleCapabilities = {
